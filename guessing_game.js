3745a3f2edc4772490e47e67d49848adc80bdd6e
justinelugli <justinelugli@gmail.com>
2015-11-04 18:55:50 -0800

    guessing game

---
+++ guessing_game.js
@@ -0,0 +1,32 @@
+	var wins = 0;
+	var losses = 0;
+
+	
+		
+function myFunction() { 
+    var number = Math.floor((Math.random() *10) +1);
+	var guess = Number(prompt("Guess a number between 1 and 10"));
+	console.log("guess", guess);
+	console.log("type of guess", typeof(guess))
+	if (guess === number) {
+		alert("Good Job! You win! The number is" + " " + number);
+		wins += 1;
+		
+	}
+
+	else if (guess !== number) { 
+		
+		alert("You Lose! the correct answer is" + " " + number);
+		losses += 1;
+		
+	};
+	console.log("losses", losses);
+	console.log("wins", wins);
+}
+
+var el = document.getElementById('wins');
+el.className = wins;
+
+var el = document.getElementById('losses');
+el.className = losses;
+	
